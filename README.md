2c96d285420394a681ea2e77b13eeea05ecd4547
Cheengizs <valinskiyartem@gmail.com>
2025-02-22 09:48:21 +0300

    0.1 vers commit A class system has been implemented and the foundation for drawing simple shapes has been laid. Need to Need to add a good GUI

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Graphic editor
+
+## Description
+In the future, the application should allow the user to draw simple shapes using the mouse, as well as taking into account color, line thickness, etc.
+
+### Versions
+- 0.1 At the moment, an approximate graphical interface of the application has been implemented. A class system has been implemented, who inherits from what. Also for the future, a function for drawing by parameters has been added.
+
---
+++ lab1proj/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1proj
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MyEllipse ellipse = new MyEllipse();
+            //ellipse.X1 = 00;
+            //ellipse.Y1 = 100;
+            //ellipse.Width = 150;
+            //ellipse.Height = 150;
+            //ellipse.LineThickness = 10;
+            //ellipse.FillColor = 0xFFAAEEBB;
+            //ellipse.LineColor = 0x00BB6969;
+            //ellipse.drawShape(canvasForDraw);
+
+            //MyPolyline polyline = new MyPolyline();
+            //polyline.LineThickness = 1;
+            //polyline.LineColor = 0xAAAAAAA;
+            //polyline.drawShape(canvasForDraw);
+
+            MyPolygon myPolygon = new MyPolygon();
+            myPolygon.LineThickness = 1;
+            myPolygon.LineColor = 0xBBCCBBCC;
+            myPolygon.FillColor = 0xBBCCBBCC;
+
+            myPolygon.drawShape(canvasForDraw);
+
+        }
+    }
+}
